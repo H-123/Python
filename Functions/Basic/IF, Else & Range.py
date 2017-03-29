714b89ef09e38d5f17d145f2ecc517b8dce6f375
MeGustaCoding <zi.huang21@gmail.com>
2017-03-28 19:59:30 -0700

    Update IF, Else & Range.py

--- Functions/Basic/IF, Else & Range.py
+++ Functions/Basic/IF, Else & Range.py
@@ -4,10 +4,10 @@
 # a%2 == 0, meaing can be divided by 2, an even number as result
 
 # "Given an integer, , perform the following conditional actions:
-# If  is odd, print Weird
-# If  is even and in the inclusive range of 2 to 5, print Not Weird
-# If  is even and in the inclusive range of 6 to 20, print Weird
-# If  is even and greater than 20, print Not Weird
+# If a is odd, print Weird
+# If a is even and in the inclusive range of 2 to 5, print Not Weird
+# If a is even and in the inclusive range of 6 to 20, print Weird
+# If a is even and greater than 20, print Not Weird
 
 
 a = int(raw_input())
