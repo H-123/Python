b5702f442556857dbe05cf733330f44620a8315c
MeGustaCoding <zi.huang21@gmail.com>
2017-03-28 18:53:18 -0700

    new folder

---
+++ Functions/Basic/Operators Decimal Places.py
@@ -0,0 +1,29 @@
+#Enter 2 numbers, to show result with and without decimal places.
+
+a = int(raw_input())
+b = int(raw_input())
+
+print (a/b)
+print float (a)/b
+
+
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print ('x / y =', float(x)/y)
+
+# Output: x // y = 3
+print ('x // y =',x//y)
+
+# Output: x ** y = 50625. 15 to power of 4
+print('x ** y =',x**y)
